c5151bcddc3eea127076ea8ff335f7bd9e017580
dollin <neil.dolling@gmail.com>
2025-05-21 20:16:43 +0100

    pattern match files in data directory

--- tax_reports/tax_reports.py
+++ tax_reports/tax_reports.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 
 from loader.holding_loader import HoldingLoader
@@ -7,14 +9,22 @@ from reporter.asset_reporter import AssetReporter
 from reporter.summary_reporter import SummaryReporter
 
 
-def execute_tax_reports(transactions_filename=None, holdings_filename=None):
-    holdings_df = HoldingLoader.load_data(transactions_filename)
-    transactions_df = TransactionLoader.load_data(holdings_filename)
-    df = pd.concat([transactions_df, holdings_df])
-    asset_summary = AssetCalculator.calculate_asset_summary(df)
+def find_csv_filename(file_pattern: str):
+    for filename in os.listdir('../data'):
+        if file_pattern in filename.lower() and filename.endswith('.csv'):
+            return filename
+    return None
+
+
+def execute_tax_reports():
+    holdings_df = HoldingLoader.load_data(find_csv_filename('holdings_report'))
+    transactions_df = TransactionLoader.load_data(find_csv_filename('capital_gains_report'))
+    df = pd.concat([holdings_df, transactions_df], ignore_index=True)
+    combined_df = df[transactions_df.columns]
+    asset_summary = AssetCalculator.calculate_asset_summary(combined_df)
     SummaryReporter.generate_summary_for_assets(asset_summary)
-    AssetReporter.generate_transaction_details_per_asset(df, asset_summary)
-    SummaryReporter.generate_overall_summary(df)
+    AssetReporter.generate_transaction_details_per_asset(combined_df, asset_summary)
+    SummaryReporter.generate_overall_summary(combined_df)
 
 
 if __name__ == "__main__":
