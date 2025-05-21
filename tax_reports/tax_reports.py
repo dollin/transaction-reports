cfca2529e9e5374ff2f348f989a213bb91d5c5d7
dollin <neil.dolling@gmail.com>
2025-05-21 20:54:45 +0100

    ability it ignore rows based on asset and wallet name

--- tax_reports/tax_reports.py
+++ tax_reports/tax_reports.py
@@ -16,16 +16,33 @@ def find_csv_filename(file_pattern: str):
     return None
 
 
-def execute_tax_reports():
+def execute_tax_reports(to_delete=None):
+    df = get_dataframe_from_reports()
+    df = delete_from_dataframe(df, to_delete)
+    asset_summary = AssetCalculator.calculate_asset_summary(df)
+    SummaryReporter.generate_summary_for_assets(asset_summary)
+    AssetReporter.generate_transaction_details_per_asset(df, asset_summary)
+    SummaryReporter.generate_overall_summary(df)
+
+
+def get_dataframe_from_reports():
     holdings_df = HoldingLoader.load_data(find_csv_filename('holdings_report'))
     transactions_df = TransactionLoader.load_data(find_csv_filename('capital_gains_report'))
     df = pd.concat([holdings_df, transactions_df], ignore_index=True)
-    combined_df = df[transactions_df.columns]
-    asset_summary = AssetCalculator.calculate_asset_summary(combined_df)
-    SummaryReporter.generate_summary_for_assets(asset_summary)
-    AssetReporter.generate_transaction_details_per_asset(combined_df, asset_summary)
-    SummaryReporter.generate_overall_summary(combined_df)
+    return df[transactions_df.columns]
+
+
+def delete_from_dataframe(df, to_delete=None):
+    if to_delete is None:
+        return df
+    for key, values in to_delete.items():
+        for value in values:
+            df = df[df[key] != str(value).upper()]
+    return df
 
 
 if __name__ == "__main__":
-    execute_tax_reports()
+    data_to_delete = {'Wallet Name': [],
+                      'Asset': ['banana', 'eth']
+                      }
+    execute_tax_reports(data_to_delete)
