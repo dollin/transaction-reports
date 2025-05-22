8ea04624a22971c2982209d62b3d94d119431c0e
dollin <neil.dolling@gmail.com>
2025-05-22 18:23:50 +0100

    remove starting holdings from asset_summary calculation

--- tax_reports/calculator/asset_calculator.py
+++ tax_reports/calculator/asset_calculator.py
@@ -2,7 +2,8 @@ class AssetCalculator:
 
     @staticmethod
     def calculate_asset_summary(df):
+        filtered_df = df[~df['Wallet Name'].str.startswith('@ ')]
         sum_columns = {'Qty': 'sum', 'Cost': 'sum', 'Proceeds': 'sum', 'Gain/Loss': 'sum'}
-        asset_summary = df.groupby('Asset').agg(sum_columns).reset_index()
+        asset_summary = filtered_df.groupby('Asset').agg(sum_columns).reset_index()
         asset_summary['Gain/Loss %'] = (asset_summary['Gain/Loss'] / asset_summary['Cost'] * 100).round(2)
         return asset_summary.sort_values('Gain/Loss', ascending=False)
